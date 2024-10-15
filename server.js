30f0ce9dba55c4e400dae08d04741dee529f942c
Watson Albert <wr971024@gmail.com>
2024-10-14 20:40:44 -0700

    Beautifycode update

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8001;
 
+const fs = require('fs');
+const beautify = require('beautify');
+
+const fileText = fs.readFileSync('./styles.css', 'utf8');
+var beautified = beautify(fileText, {format: "css"});
+
+fs.writeFileSync('beautified.css', beautified);
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
