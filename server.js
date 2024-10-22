8afc97fc38504ebbd57594a3e635ab8b7f0cc73f
Watson Albert <wr971024@gmail.com>
2024-10-21 20:13:29 -0700

    chore: fix server.js file

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ const port = 8001;
 
 const Router = require('./routes/api');
 
-mongoose.connect("mongodb://127.0.0.1:27017/beautify")
-    .then((err) => {
-        if (err) console.log('Error, ' + err);
-        else console.log("Successfully connected!");
+mongoose.connect("mongodb://localhost:27017/beautify", {
+    directConnection: true,
+    serverSelectionTimeoutMS: 2000,
+    appName: 'mongosh'
+})
+    .then(() => {
+        console.log("Successfully connected!");
+    })
+    .catch(err => {
+        console.log(err);
     });
 
 app.use('/api', Router);
@@ -23,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
  * @func app.listen
  * @description 'example func'
  * @param port 'params0'
- * @param ( 'params1'
+ * @param ( 'params1' )
  */
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
